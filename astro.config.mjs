162692df1f146af62a3ee7b6472246b512b02cfe
Kyle Butts <buttskyle96@gmail.com>
2022-08-05 12:08:48 -0600

    Switch to mdx pt. 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,42 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import rehypeCodeTitles from "rehype-code-titles";
-import rehypeKatex from "rehype-katex";
-import remarkMath from "remark-math";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import react from "@astrojs/react"
+import rehypeCodeTitles from "rehype-code-titles"
+import rehypeKatex from "rehype-katex"
+import remarkMath from "remark-math"
+import mdx from "@astrojs/mdx"
+
+import image from "@astrojs/image"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      config: { applyBaseStyles: false },
+	integrations: [
+		tailwind({
+			config: {
+				applyBaseStyles: false,
+			},
+		}),
+		react(),
+		mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: { extends: [rehypeCodeTitles, rehypeKatex] },
+      syntaxHighlight: "shiki",
+      shikiConfig: {
+        theme: "rose-pine",
+      },
     }),
-    react()
-  ],
+		image(),
+	],
   markdown: {
-    remarkPlugins: [
-      remarkMath,
-    ],
-    rehypePlugins: [
-      rehypeCodeTitles,
-      rehypeKatex,
-    ],
-    syntaxHighlight: 'shiki',
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeCodeTitles, rehypeKatex],
+    syntaxHighlight: "shiki",
     shikiConfig: {
-      theme: 'rose-pine',
-    }
+      theme: "rose-pine",
+    },
   },
-  outDir: "./docs",
-});
+	outDir: "./docs",
+  legacy: {
+    astroFlavoredMarkdown: true
+  },
+})
