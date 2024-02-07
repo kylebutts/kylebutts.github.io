c7aa231a864aa4c0df522655e2c625ae6fba7266
Kyle Butts <buttskyle96@gmail.com>
2024-02-07 14:19:36 -0500

    Fix Netlify deployment isuse

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,21 @@
-import { defineConfig } from "astro/config"
-import tailwind from "@astrojs/tailwind"
-import remarkMath from "remark-math"
-import rehypeKatex from "rehype-katex"
-import mdx from "@astrojs/mdx"
-import solid from "@astrojs/solid-js"
-import sitemap from "@astrojs/sitemap"
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import mdx from "@astrojs/mdx";
+import solid from "@astrojs/solid-js";
+import sitemap from "@astrojs/sitemap";
+
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.kylebutts.com/',
-	integrations: [
-		mdx({
-			remarkPlugins: [remarkMath],
-			rehypePlugins: [rehypeKatex],
-			syntaxHighlight: "shiki",
-			shikiConfig: {
-				theme: "vitesse-light",
-			},
-		}),
-		tailwind(),
-		solid(),
-		sitemap(),
-	],
-})
+  site: 'https://www.kylebutts.com/',
+  integrations: [mdx({
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "vitesse-light"
+    }
+  }), tailwind(), solid()]
+});
