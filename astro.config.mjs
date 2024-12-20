02988fae335c0a0f7aa78f4442e367874ce5d4a4
Kyle Butts <buttskyle96@gmail.com>
2024-12-19 18:10:00 -0600

    Fix publishing bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,26 +4,28 @@ import mdx from "@astrojs/mdx"
 import remarkMath from "remark-math"
 import rehypeKatex from "rehype-katex"
 
+import solidJs from "@astrojs/solid-js";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.kylebutts.com/",
-	markdown: {
-		syntaxHighlight: "shiki",
-		shikiConfig: {
-			theme: "vitesse-light",
-		},
-		remarkPlugins: [remarkMath],
-		rehypePlugins: [rehypeKatex],
-		// rehypePlugins: [
-		// 	[
-		// 		rehypeKatex,
-		// 		{
-		// 			// Katex plugin options
-		// 			fleqn: true,
-		// 		},
-		// 	],
-		// ],
-		gfm: true,
-	},
-	integrations: [mdx({}), tailwind()],
+    site: "https://www.kylebutts.com/",
+    markdown: {
+        syntaxHighlight: "shiki",
+        shikiConfig: {
+            theme: "vitesse-light",
+        },
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeKatex],
+        // rehypePlugins: [
+        // 	[
+        // 		rehypeKatex,
+        // 		{
+        // 			// Katex plugin options
+        // 			fleqn: true,
+        // 		},
+        // 	],
+        // ],
+        gfm: true,
+    },
+    integrations: [mdx({}), tailwind(), solidJs()],
 })
