bb6f5d3a45f64ff0d1ef59bf038d02d3691c8ee0
Kyle Butts <buttskyle96@gmail.com>
2025-03-04 18:12:26 -0600

    Updates!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,41 @@
 import { defineConfig } from "astro/config"
-import tailwind from "@astrojs/tailwind"
+import tailwindcss from "@tailwindcss/vite"
 import solidJs from "@astrojs/solid-js"
 import mdx from "@astrojs/mdx"
 import remarkMath from "remark-math"
 import rehypeKatex from "rehype-katex"
+import astroExpressiveCode from "astro-expressive-code"
+import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections'
+import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
+
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://www.kylebutts.com/",
-    markdown: {
-        syntaxHighlight: "shiki",
-        shikiConfig: {
-          theme: "vitesse-light",
-        },
-        remarkPlugins: [remarkMath],
-        rehypePlugins: [rehypeKatex],
-        gfm: true,
-    },
-    integrations: [mdx(
+	site: "https://www.kylebutts.com/",
+	integrations: [
+		// expressiveCode(),
+    astroExpressiveCode({
+      themes: ['vitesse-light'],
+      plugins: [pluginCollapsibleSections(), pluginLineNumbers()],
+      shiki: {
+        langs: [
+          JSON.parse(fs.readFileSync('./r.tmLanguage.gen.json', 'utf-8'))
+        ],
+      },
+    }),
+		mdx(),
+		solidJs(),
+	],
 
-    ), tailwind(), solidJs()],
+	plugins: [tailwindcss()],
+
+	markdown: {
+		// syntaxHighlight: "shiki",
+		// shikiConfig: { theme: "vitesse-light" },
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+		gfm: true,
+	},
+
+	vite: { plugins: [tailwindcss()] },
 })
--- src/components/mdx/index.js
+++ src/components/mdx/index.js
@@ -1,9 +1,6 @@
-import Bg from "./Bg.astro"
-import Color from "./Color.astro"
 import Figure from "./Figure.astro"
 import LargeBlock from "./LargeBlock.astro"
-import LinkBlock from "./LinkBlock.astro"
 
 export {
-  Bg, Color, Figure, LinkBlock, LargeBlock
+  Figure, LargeBlock
 }
